84c1eb75c18b7b063c55420921ea718725a1d868
Carlos Henrique Gonçalves Batista <habeenkii_13@outlook.com.br>
2024-01-27 19:12:28 -0300

    Renderização da homepage adicionada

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__, template_folder='view') #por default use o nome da pasta templates 
-
-from app import admin
-from app import cliente
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, request, render_template, make_response 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='view') 
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('homepage.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
