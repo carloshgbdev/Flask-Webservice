ba412e0f66f857e9ecd05b7167843ce7a9271078
Carlos Henrique Gonçalves Batista <habeenkii_13@outlook.com.br>
2024-01-28 18:46:27 -0300

    Criação da coleção Clientes e upload para o Firebase

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import firebase_admin
 import random
+import os
 
-from flask import Flask, request, render_template, make_response 
+from flask import Flask, request, render_template, make_response, session
 from firebase_admin import credentials, firestore
 
 app = Flask(__name__, template_folder='view') 
+app.secret_key = os.urandom(24)
 
 cred = credentials.Certificate("firebase.json")
 firebase_admin.initialize_app(cred)
@@ -33,16 +35,75 @@ def adicionar_empresa_cliente(nome, tipo, rua, cep, complemento, cnpj, producao,
         'status': 'Ativo'
     })
 
-def busca_colecao(colecao):
-    empresas_clientes_ref = db.collection(colecao)
-    docs = empresas_clientes_ref.stream()
+def adicionar_cliente(cnpj, nome, tipo, rua, cep, complemento, cpf_cnpj, telefone, consumo, energia_alocada):
+    clientes_ref = db.collection('Clientes')
+    # O Firestore criará automaticamente um ID único para cada novo documento
+    cliente_ref = clientes_ref.document()
     
-    empresas_clientes = []
+    # Definir o documento com os dados da empresa
+    cliente_ref.set({
+        'nome': nome,
+        'tipo': tipo,
+        'rua': rua,
+        'cep': cep,
+        'complemento': complemento,
+        'cpf_cnpj': cpf_cnpj,
+        'telefone': telefone,
+        'consumo': consumo,
+        'energia_alocada': energia_alocada,
+        'status': 'Ativo'
+    })
+    
+    lista_clientes = puxa_clientes_da_empresa(cnpj)
+    
+    lista_clientes.append(cliente_ref)
+    empresa_ref = busca_primeiro_documento('Empresas Clientes', 'cnpj', cnpj)
+    
+    empresa_ref.update({
+        'clientes': lista_clientes
+    })
+
+def puxa_clientes_da_empresa(cnpj):
+    empresas_clientes_ref = db.collection('Empresas Clientes')
+    
+    query = empresas_clientes_ref.where('cnpj', '==', cnpj)
+    
+    resultados = query.stream()
+    
+    empresas = []
+    
+    for resultado in resultados:
+        empresas.append(resultado.to_dict())
+    
+    empresa_alvo = empresas[0]
+    
+    if "clientes" in empresa_alvo:
+        return empresa_alvo['clientes']
+    else:
+        return []
+
+def busca_colecao(colecao):
+    colecao_ref = db.collection(colecao)
+    docs = colecao_ref.stream()
+    
+    colecao = []
     for doc in docs:
         # doc.to_dict() converte o documento em um dicionário
-        empresas_clientes.append(doc.to_dict())
+        colecao.append(doc.to_dict())
 
-    return empresas_clientes
+    return colecao
+
+def busca_primeiro_documento(colecao, campo, valor):
+    empresas_clientes_ref = db.collection(colecao)
+    query = empresas_clientes_ref.where(campo, '==', valor)
+    resultados = query.stream()
+    
+    documentos = []
+    
+    for resultado in resultados:
+        documentos.append(resultado.reference)
+    
+    return documentos[0]
 
 @app.route('/', methods=['GET'])
 def home():
@@ -56,6 +117,9 @@ def login():
     if (autenticado == 'true'):
         cnpj = cookies.get('cnpj')
         key = cookies.get('key')
+        
+        session['cnpj'] = cnpj
+        session['key'] = key
         
         if (cnpj == 'admin' and key == 'admin'):
             resp = make_response(adm())
@@ -96,6 +160,9 @@ def autenticacao():
             
             for empresa in empresas:
                 if (cnpj == empresa['cnpj'] and key == empresa['acess']):
+                    session['cnpj'] = cnpj
+                    session['key'] = key
+        
                     if (remember == 'on'):
                         resp.set_cookie('cnpj', cnpj, max_age=30)
                         resp.set_cookie('key', key, max_age=30)
@@ -155,12 +222,43 @@ def cliente_config():
     return render_template('clienteconfig.html')
 
 @app.route('/pagina-cliente/adicao-clientes', methods=['GET', 'POST'])
-def adicao_clientes():
-    return render_template('adicaoclientes.html')
+def adicao_clientes(msg=None):
+    if (msg == None):
+        return render_template('adicaoclientes.html')
+    else:
+        return render_template('adicaoclientes.html', msg=msg)
+
+@app.route('/pagina-cliente//adicao-clientes/cliente', methods=['POST'])
+def registra_cliente():
+    nome = request.form['name']
+    tipo = request.form['type']
+    rua = request.form['rua']
+    cep = request.form['cep']
+    complemento = request.form['complemento']
+    cpf_cnpj = request.form['cpf_cnpj']
+    telefone = request.form['telefone']
+    consumo = request.form['consumo_mensal']
+    energia_alocada = request.form['energia_alocada']
+    
+
+    adicionar_cliente(nome=nome, tipo=tipo, 
+                    rua=rua, cep=cep, 
+                    complemento=complemento, cpf_cnpj=cpf_cnpj, 
+                    telefone=telefone, consumo=consumo, 
+                    energia_alocada=energia_alocada, cnpj=session.get('cnpj'))
+    
+    resp = make_response(adicao_clientes(msg="<p style=\"color: green\">Cliente adicionado com sucesso!</p>"))
+    
+    return resp
+
+
 
 @app.route('/pagina-cliente/filtro-clientes', methods=['GET'])
-def filtro_clientes():
-    return render_template('filtroclientes.html')
+def filtro_clientes(msg=None):
+    if (msg == None):
+        return render_template('filtroclientes.html')
+    else:
+        return render_template('filtroclientes.html', msg=msg)
 
 @app.route('/pagina-cliente/filtro-clientes/filtrados', methods=['GET'])
 def filtrados():
