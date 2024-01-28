2084f3d21f2b603be1789ab77e5c1d249e81f2ac
Carlos Henrique Gonçalves Batista <habeenkii_13@outlook.com.br>
2024-01-28 14:23:30 -0300

    Leitura e adição de empresas clientes no Firebase

--- main.py
+++ main.py
@@ -10,21 +10,38 @@ firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
+def adicionar_empresa_cliente(nome, tipo, rua, cep, complemento, cnpj, producao, consumo):
+    empresas_clientes_ref = db.collection('Empresas Clientes')
+    # O Firestore criará automaticamente um ID único para cada novo documento
+    empresa_cliente_ref = empresas_clientes_ref.document()
+    
+    # Definir o documento com os dados da empresa
+    empresa_cliente_ref.set({
+        'nome': nome,
+        'tipo': tipo,
+        'rua': rua,
+        'cep': cep,
+        'complemento': complemento,
+        'cnpj': cnpj,
+        'producao': producao,
+        'consumo': consumo,
+        'status': 'Ativo'
+    })
+
+def busca_empresas_clientes():
+    empresas_clientes_ref = db.collection('Empresas Clientes')
+    docs = empresas_clientes_ref.stream()
+    
+    empresas_clientes = []
+    for doc in docs:
+        # doc.to_dict() converte o documento em um dicionário
+        empresas_clientes.append(doc.to_dict())
+
+    return empresas_clientes
+
 @app.route('/', methods=['GET'])
 def home():
     return render_template('homepage.html')
-
-@app.route('/adm', methods=['GET', 'POST'])
-def adm():
-    return render_template('adm.html')
-
-@app.route('/sobre-nos', methods=['GET'])
-def sobre_nos():
-    return render_template('sobrenos.html')
-
-@app.route('/contato', methods=['GET', 'POST'])
-def contato():
-    return render_template('contato.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -36,9 +53,8 @@ def login():
         key = cookies.get('key')
         
         if (cnpj == 'admin' and key == 'admin'):
-            resp = make_response(render_template('adm.html',
-                                                 acess=cnpj,
-                                                 key=key))
+            empresas = busca_empresas_clientes()
+            resp = make_response(adm(empresas=empresas))
             
             return resp
         else:
@@ -49,6 +65,70 @@ def login():
             return resp
         
     return render_template('login.html')
+
+@app.route('/login/autenticacao', methods=['POST'])
+def autenticacao():
+    cnpj = request.form['cnpj']
+    key = request.form['key']
+    remember = "off"
+    
+    try:
+        remember = request.form['remember']
+    except:
+        remember = "off"
+    
+    if (cnpj == 'admin' and key == 'admin'):
+        empresas = busca_empresas_clientes()
+        resp = make_response(adm(empresas=empresas))
+        
+        if (remember == 'on'):
+            resp.set_cookie('cnpj', cnpj, max_age=30)
+            resp.set_cookie('key', key, max_age=30)
+            resp.set_cookie('autenticado', 'true', max_age=30)
+        
+        return resp
+    else:
+        return render_template('login.html', msg_err_autenticacao='Erro na autenticação!')
+    
+@app.route('/adm', methods=['GET', 'POST'])
+def adm(empresas, msg=None, acess='admin', key='admin'):
+    if (msg == None):
+        return render_template('adm.html', empresas=empresas, acess=acess, key=key)
+    else:
+        return render_template('adm.html', empresas=empresas, acess=acess, key=key, msg=msg)
+
+@app.route('/adm/adiciona-empresa', methods=['POST'])
+def adiciona_empresa_cliente():
+    nome = request.form['nome']
+    tipo = request.form['tipo']
+    rua = request.form['rua']
+    cep = request.form['cep']
+    complemento = request.form['complemento']
+    cnpj = request.form['cnpj']
+    
+    try:
+        adicionar_empresa_cliente(nome=nome, cep=cep, 
+                                tipo=tipo, rua=rua, 
+                                complemento=complemento, cnpj=cnpj, 
+                                producao='N/E', consumo='N/E')
+        
+        empresas = busca_empresas_clientes()
+        resp = make_response(adm(empresas=empresas, 
+                                 msg="<p style=\"color: green\">Cliente adicionado com sucesso!</p>"))
+        
+        return resp
+    except:
+        empresas = busca_empresas_clientes()
+        return adm(empresas=empresas,
+                   msg="<p style=\"color: red\">Erro ao adicionar o cliente</p>")
+
+@app.route('/sobre-nos', methods=['GET'])
+def sobre_nos():
+    return render_template('sobrenos.html')
+
+@app.route('/contato', methods=['GET', 'POST'])
+def contato():
+    return render_template('contato.html')
 
 @app.route('/paginacliente', methods=['GET'])
 def paginacliente():
@@ -74,28 +154,5 @@ def filtrados():
 def fatura():
     return render_template('fatura.html')
 
-@app.route('/login/autenticacao', methods=['POST'])
-def autenticacao():
-    cnpj = request.form['cnpj']
-    key = request.form['key']
-    remember = "off"
-    
-    try:
-        remember = request.form['remember']
-    except:
-        remember = "off"
-    
-    if (cnpj == 'admin' and key == 'admin'):
-        resp = make_response(render_template('adm.html', acess=cnpj, key=key))
-        
-        if (remember == 'on'):
-            resp.set_cookie('cnpj', cnpj, max_age=30)
-            resp.set_cookie('key', key, max_age=30)
-            resp.set_cookie('autenticado', 'true', max_age=30)
-        
-        return resp
-    else:
-        return render_template('login.html', msg_err_autenticacao='Erro na autenticação!')
-    
 if __name__ == '__main__':
     app.run(debug=True)
